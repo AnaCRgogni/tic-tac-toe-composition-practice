f059089a87eda0095a90877758fb727dcecc7681
AnaCRgogni <106693889+AnaCRgogni@users.noreply.github.com>
2025-07-22 21:44:56 -0600

    Add human player turn logic

--- game.js
+++ game.js
@@ -10,6 +10,7 @@ const BoardModule = (function () {
     // en JavaScript, no cuando se usa el patrón de módulo con IIFE y objetos literales.
     const boardProto = {
         logBoard() {
+            // Solo se usa 'this' dentro de métodos que están en el prototipo
             this.matrix.forEach(row => console.log(row));
         }
     };
@@ -20,6 +21,7 @@ const BoardModule = (function () {
         for (let i = 0; i < 3; i++) {
             matrix[i] = [];
             for (let j = 0; j < 3; j++) {
+                // Row - Column
                 matrix[i][j] = ' ';
             }
         }
@@ -27,6 +29,7 @@ const BoardModule = (function () {
         // Crea el objeto board y le asigna el prototipo
         const board = Object.create(boardProto);
         board.matrix = matrix;
+        // board is the object and matrix is a property inside of it
         return board;
     }
 
@@ -59,12 +62,44 @@ const PlayerModule = (function () {
 
 const LogicModule = (function () {
 
-    const logicProto = {
+    // No crea objetos por lo que no necesita un prototipo
 
-    };
+    function humanTurn(board, player) {
+        let i, j;
+        do {
+            i = parseInt(prompt('Escribe la fila (0-2):'));
+            // Si no es un numero, es menor que 0 o mayor que 2, siga pidiendo el prompt
+        } while (isNaN(i) || i < 0 || i > 2);
+
+        do {
+            j = parseInt(prompt('Escribe la columna (0-2):'));
+        } while (isNaN(j) || j < 0 || j > 2);
+
+        board.matrix[i][j] = player.markerType;
+    }
+
+    function checkMatrixRows() {
+
+    }
+
+    function checkMatrixDiagonals() {
+        
+    }
+
+    function machineTurn(board, player) {
+        let i, j;
+
+    }
+
+    function checkWinner() {
+
+    }
 
     return {
-
+        humanTurn,
+        machineTurn,
+        checkMarkCount,
+        checkWinner
     };
 })();
 
@@ -76,3 +111,7 @@ machinePlayer.logPlayer();
 const myBoard = BoardModule.createBoard();
 // Este metodo esta disponible por prototipo, no por closures
 myBoard.logBoard(); 
+
+LogicModule.humanTurn(myBoard, humanPlayer);
+myBoard.logBoard();
+LogicModule.machineTurn(myBoard, machinePlayer);
